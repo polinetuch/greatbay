5c130538a8fab2c26725e9aa3f50c18c40622143
Polinet <uchpolinet@gmail.com>
2019-08-30 23:42:53 +1000

    used inquirer.prompt to create questions for postItem function

--- greatbayCRUD.js
+++ greatbayCRUD.js
@@ -1,26 +1,27 @@
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
+// var connection = mysql.createConnection({
+//   host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+//   // Your port; if not 3306
+//   port: 3306,
 
-  // Your username
-  user: "root",
+//   // Your username
+//   user: "root",
 
-  // Your password
-  password: "root",
-  database: "greatbay_db"
-});
+//   // Your password
+//   password: "root",
+//   database: "greatbay_db"
+// });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  connection.end();
-});
+// connection.connect(function(err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId);
+//   connection.end();
+// });
 
 var inquirer = require("inquirer");
+var postedItemArr = [];
 
 // function to start Great Bay by choosing one of the options
 var startGB = function() {
@@ -48,16 +49,31 @@ var startGB = function() {
 startGB();
 
 var postItem = function() {
+  inquirer
+    .prompt([
+      {
+        name: "POST",
+        type: "input",
+        message: "What item would you like to post?"
+      },
+      {
+        name: "category",
+        type: "input",
+        message: "Which category does it belong to?"
+      }
+    ])
+    .then(function(response) {
+      console.log(response);
+      postedItemArr.push(response.POST);
+    });
+};
+
+var bidItem = function() {
   inquirer.prompt([
     {
-      name: "POST",
-      type: "input",
-      message: "What item would you like to post?"
-    },
-    {
-      name: "category",
-      type: "input",
-      message: "Which category does it belong to?"
+      name: "BID",
+      message: "Which item would you like to bid?",
+      choices: answer.postItem
     }
   ]);
 };
